3de379949c1e2d1bd1a527d247ee60b0ecbf53a8
qianshupeng <qianshup@usc.edu>
2026-01-27 23:23:16 -0800

    setup

---
+++ convolution_cpu.c
@@ -0,0 +1,19 @@
+// convolution on CPU implementation
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// TODO: Define several different kernels here
+
+// TODO: Implement the convolution function here
+void convolution2D(unsigned int *input, unsigned int *output, int M, float kernel, int N) {
+    // M = image size M*M
+    // N = kernel size N*N
+    // TODO: Convolution implementation
+}
+
+// TODO: Test function with timing
+
+int main() {
+    // Test image, conv, timing, output
+}
